9e67bc550ff9bde7db0e597b8fd2d237d65483dd
sc0ncept <sebconcept@outlook.com>
2021-01-07 21:03:52 +0100

    Add course solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let storagePrefix = 'from-autosave_';
+let storageID = 'from-autosave';
 
 const getId = function (input) {
   if (input.id.length > 0) {
@@ -13,48 +13,39 @@ const getId = function (input) {
 }
 
 const loadData = function () {
+
+  let saved = localStorage.getItem(storageID);
+  if (!saved) return;
+  saved = JSON.parse(saved);
+
   const inputs = document.querySelectorAll('#save-me input, #save-me textarea');
 
   Array.prototype.slice.call(inputs).forEach(function (input) {
 
     const id = getId(input);
     if (!id) return;
+    if (!saved[id]) return;
 
-    const saved = localStorage.getItem(storagePrefix + id);
-    if (!saved) return;
-
-    input.value = saved;
+    input.value = saved[id];
   })
-}
-
-const clearData = function () {
-  const inputs = document.querySelectorAll('#save-me input, #save-me textarea');
-
-  Array.prototype.slice.call(inputs).forEach(function (input) {
-
-    const id = getId(input);
-    if (!id) return;
-
-    localStorage.removeItem(storagePrefix + id);
-  })
-
 }
 
 const inputHandler = function (event) {
   if (!event.target.closest('#save-me')) return;
-
   const id = getId(event.target);
-
   if (!id) return;
 
-  localStorage.setItem(storagePrefix + id, event.target.value);
+  let saved = localStorage.getItem(storageID);
+  saved = saved ? JSON.parse(saved) : {};
+  saved[id] = event.target.value;
+  localStorage.setItem(storageID, JSON.stringify(saved));
 };
 
 const submitHandler = function (event) {
   event.preventDefault();
 
   if (event.target.id !== 'save-me') return;
-  clearData();
+  localStorage.removeItem(storageID);
 }
 
 loadData();
